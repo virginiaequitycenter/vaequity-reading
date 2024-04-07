6a33641583205329e0faa9a539a1c4f2fe760778
Samantha Toet <samanthactoet@gmail.com>
2024-04-07 17:41:30 -0400

    reactive vales

--- tornado/app.R
+++ tornado/app.R
@@ -22,10 +22,10 @@ ui <- page_fillable(
                          label = "Select a Demographic to Examine",
                          choices = unique(tornado$demographic),
                          selected = NULL),
-             #selectInput(inputID = "division", 
-                         #label = "Select a Division to Highlight",
-                         #choices = unique(tornado$division_name),
-                         #selected = NULL),
+             selectInput(inputId = "division",
+             label = "Select a Division to Highlight",
+             choices = unique(tornado$division_name),
+             selected = NULL),
              sliderInput("year", "Select Year:", 
                          min = min(tornado$test_year), max = max(tornado$test_year), value = 2022, step = 1, sep =''),
       ),
@@ -34,7 +34,7 @@ ui <- page_fillable(
     # Row for plot 
     fluidRow(
       column(12, align = "center",
-             plotlyOutput("tornadoPlot"),
+             plotlyOutput("tornadoPlot", height='800px'),
         )
     )
 )
@@ -42,21 +42,27 @@ ui <- page_fillable(
 # Server ----
 # Define server logic required to create the tornado 
 server <- function(input, output) {
+  
+  rvs = reactiveValues()
+  
+  observeEvent(c(input$year, input$demo), {
+    rvs$plt_dat = tornado %>% 
+      filter(demographic == input$demo, test_year == input$year) %>% 
+      mutate(division_name = fct_reorder(division_name, desc(rank)))
+  })
 
-    output$tornadoPlot <- renderPlotly({
-
-        # draw the plot with the specified demographics and year 
-      tornado %>% 
-        filter(demographic == input$demo, test_year == input$year) %>% 
-        mutate(division_name = fct_reorder(division_name, desc(rank))) %>%
-        ggplot(aes(y = division_name, x = rate, group = division_name, color = level)) +
-        geom_path(color = "grey") +
-        geom_point() +
-        labs(x ="", y = "School Division") +  
-        theme_classic() +
-        theme(axis.text.y = element_text(size = 2))
-      
-    })
+  output$tornadoPlot <- renderPlotly({
+    # draw the plot with the specified demographics and year 
+    ggplot(rvs$plt_dat, aes(y = division_name, x = rate, group = division_name, color = level)) +
+    geom_path(color = "grey") +
+    geom_path(data = filter(rvs$plt_dat, division_name == input$division), color = "black", size = 1) +
+    geom_point() +
+    labs(x ="", y = "School Division") +  
+    theme_classic() +
+    theme(axis.text.y = element_text(size =3)) +
+    labs(colour = NULL)
+    
+  })
 }
 
 # Run the application 
