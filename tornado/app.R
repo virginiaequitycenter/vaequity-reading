70b23f8cdc573f8d78280e05d4741389cb0bb466
Samantha Toet <samanthactoet@gmail.com>
2024-04-25 13:07:13 -0400

    modify tooltip

--- tornado/app.R
+++ tornado/app.R
@@ -13,7 +13,10 @@ tornado <- read_csv("final_reading_tornado.csv")
 ui <- page_fillable(
 
     # Application title
-    titlePanel("SOL Reading Gaps by School Division"),
+    titlePanel(h1("Virginia's Reading SOL Gaps: Divisions, 2006-2023", align = "center")),
+    
+    #Subtitle
+    fluidRow(h6("Sam Powers, Michele Claibourn, and Samantha Toet", align = "center")),
     
     # Row for user selections 
     fluidRow(
@@ -27,7 +30,7 @@ ui <- page_fillable(
              choices = unique(tornado$division_name),
              selected = NULL),
              sliderInput("year", "Select Year:", 
-                         min = min(tornado$test_year), max = max(tornado$test_year), value = 2022, step = 1, sep =''),
+                         min = min(tornado$test_year), max = max(tornado$test_year), value = 2023, step = 1, sep =''),
       ),
     ),
     
@@ -52,16 +55,22 @@ server <- function(input, output) {
   })
 
   output$tornadoPlot <- renderPlotly({
-    # draw the plot with the specified demographics and year 
-    ggplot(rvs$plt_dat, aes(y = division_name, x = rate, group = division_name, color = level)) +
+    
+    tornado_plot <- ggplot(rvs$plt_dat, aes(y = division_name, x = rate, group = division_name, color = level,
+                                            text = paste0("Divison: ", division_name, 
+                                                          "\nDemographic: ", level,
+                                                          "\nPass Rate: ", scales::percent(rate, accuracy = 0.1)))) +
     geom_path(color = "grey") +
     geom_path(data = filter(rvs$plt_dat, division_name == input$division), color = "black", size = 1) +
     geom_point() +
     labs(x ="", y = "School Division") +  
     theme_classic() +
     theme(axis.text.y = element_text(size =3)) +
-    labs(colour = NULL)
+    labs(colour = NULL) +
+    scale_x_continuous(labels = scales::percent) +
+    geom_vline(xintercept = c(0.4, 0.6, 0.8), linetype = "dotted", color = "light grey") 
     
+    ggplotly(tornado_plot, tooltip = "text")
   })
 }
 
