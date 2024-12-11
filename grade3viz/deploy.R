15283794b639f2dc8fe7d39babac4e61913a0fdc
Samantha Toet <samanthactoet@gmail.com>
2024-12-11 17:21:03 -0500

    Delete grade3viz directory

--- grade3viz/deploy.R
+++
@@ -1,9 +0,0 @@
-rsconnect::deployApp(appName = "grade3viz", 
-                     appFiles = c("app.R", 
-                                  "www/thirdgrade_alllstudents.csv"),
-                     account = "virginiaequitycenter") 
-
-# rsconnect::deployApp(appName = "grade3viz", 
-#                      appFiles = c("app.R", 
-#                                   "www/thirdgrade_alllstudents.csv"),
-#                      account = "commpaslab") 
